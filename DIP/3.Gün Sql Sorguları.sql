eadb7534cf01839a1eb69918f35816bf75bdd263
mberkayakar <m.berkay.akar@gmail.com>
2021-08-12 11:58:35 +0300

    4.Gün eklendi

---
+++ DIP/3.Gün Sql Sorguları.sql
@@ -0,0 +1,47 @@
+﻿/*   Veri tabanı dosyası oluşturuldu   */
+create database DIP_BERKAY_AKAR_STAJ
+
+
+/* Veri tabanının kullanılabilmesi için Use ile seçilmesi gerekti*/
+USE DIP_BERKAY_AKAR_STAJ
+
+
+/*	DIP_Employees tablosunu oluşturan script	*/
+
+
+
+
+
+create table DIP_Employees(
+TCKN  decimal(11) primary key,  
+Ad nvarchar(50) not null,
+Soyad nvarchar(50) not null,
+Dogum_Tarihi date not null ,
+telefon_numarası_1 decimal (10),
+telefon_numarası_2 decimal (10),
+)
+
+
+
+
+/*			2.)  Tablo içerisine Personel Ekleme         */
+
+insert into DIP_Employees values (1,'Berkay','Akar','1999-05-25',5415975442,5526931436)
+insert into DIP_Employees values (2,'Anıl','Akar','1984-07-25',5415975442,5526931436)
+insert into DIP_Employees values (3,'Burak','Karaca','1980-12-05',5415975442,5526931436)
+
+
+/* Soru 1 : En küçük elemanın bilgileri */
+
+
+
+/* Soru 2 : En küçük ve en büyük elemanın bilgileri aynı anda getir  */
+
+-- Bu işlem için union kullanılması gerekmektedir
+
+
+SELECT * from DIP_Employees  where Dogum_Tarihi=(SELECT MIN(Dogum_Tarihi) from DIP_Employees) 
+union
+SELECT * from DIP_Employees  where Dogum_Tarihi=(SELECT MAX(Dogum_Tarihi) from DIP_Employees) 
+
+
---
+++ DIP/4.Gün Sql Sorguları.sql
@@ -0,0 +1,35 @@
+﻿use DIP_BERKAY_AKAR_STAJ
+
+
+/*	DIP_Employees tablosunu oluşturan script	*/
+
+create table DIP_Employees(
+TCKN  decimal(11) primary key,  
+Ad nvarchar(50) not null,
+Soyad nvarchar(50) not null,
+Dogum_Tarihi date not null ,
+telefon_numarası_1 decimal (10),
+telefon_numarası_2 decimal (10),
+)
+
+/*			1.)  Maaş Tablosu Oluşturma         */
+
+
+create table CalısanMaas(
+ID int identity(1,1) primary key,  -- Normalizasyon kuralları geregi oluturulmuştur
+TCKN decimal(11),
+Tarih date not null ,
+Maas decimal(10,2) not null,
+CONSTRAINT FK_maasbirlestirmesi FOREIGN KEY (TCKN) REFERENCES DIP_Employees(TCKN),  
+)
+
+ 
+
+
+
+
+/* Soru 2 : Eleman isimlerini Ocak maaşına göre azalan sıralayınız.  */
+
+select CalısanMaas.TCKN, Ad,Soyad,Dogum_Tarihi,Tarih,Maas from DIP_Employees inner join CalısanMaas on CalısanMaas.TCKN = DIP_Employees.TCKN where CalısanMaas.Tarih between '2021-01-01' AND '2021-01-31' 
+order by Maas Desc
+
